9df573d373456ea0ee95730d4aa28fa11fc168cf
zoubinthw <316324334@qq.com>
2023-11-04 23:39:08 +0800

    [bin] feat: add test api

---
+++ src/main/java/com/example/demo/web/TestController.java
@@ -0,0 +1,18 @@
+package com.example.demo.web;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+    @GetMapping
+    public ResponseEntity<Map<String, String>> test() {
+        return ResponseEntity.ok().body(Map.of("Testing", "Up and runing"));
+    }
+}
