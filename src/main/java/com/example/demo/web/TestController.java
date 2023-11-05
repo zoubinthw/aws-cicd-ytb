bbdbb63e2f2bb879eb3e4c25ad909aaa22b28794
zoubinthw <316324334@qq.com>
2023-11-05 15:03:31 +0800

    [bin] feat: add ip address.

--- src/main/java/com/example/demo/web/TestController.java
+++ src/main/java/com/example/demo/web/TestController.java
@@ -6,13 +6,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.Inet4Address;
+import java.net.UnknownHostException;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/test")
 public class TestController {
     @GetMapping
-    public ResponseEntity<Map<String, String>> test() {
-        return ResponseEntity.ok().body(Map.of("Testing", "Up and runing"));
+    public ResponseEntity<Map<String, String>> test() throws UnknownHostException {
+        String hostAddress = Inet4Address.getLocalHost().getHostAddress();
+        return ResponseEntity.ok().body(Map.of("Testing", "Up and running: " + hostAddress));
     }
 }
